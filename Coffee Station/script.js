56c22c3b997035d8f64ee3debc848073b1ed49cf
Sirhid24k <abdulateefsaheed7@gmail.com>
2023-11-12 02:08:02 +0100

    first commit

---
+++ Coffee Station/script.js
@@ -0,0 +1,57 @@
+const nav = document.querySelector('.nav');
+const showNav = document.querySelector('.show-nav-btn');
+const hideNav = document.querySelector('.hide-nav-btn');
+const navLinks = document.querySelectorAll('.nav-link');
+const headerLinks = document.querySelectorAll('.header-link');
+const headerTab = document.querySelector('.header-tab');
+const date = document.querySelector('.date');
+
+// Get Date
+date.textContent = new Date().getFullYear();
+
+// Show/Hide Navbar
+showNav.addEventListener('click', () => {
+  nav.classList.add('hidden');
+});
+hideNav.addEventListener('click', () => {
+  nav.classList.remove('hidden');
+});
+
+// Show/Hide Header-tab
+const hideHeadertab = () => {
+  headerTab.style.transform = 'translateY(-100%)';
+};
+const showHeadertab = () => {
+  headerTab.style.transform = 'translateY(0)';
+};
+
+// Smoothscrolling
+headerLinks.forEach(head => {
+  head.addEventListener('click', e => {
+    e.preventDefault();
+    const clicked = e.target.getAttribute('href');
+    const scrollEl = document.querySelector(clicked);
+    scrollEl.scrollIntoView({ behavior: 'smooth' });
+    hideHeadertab();
+  });
+});
+
+let scrollPosition = window.scrollY;
+window.addEventListener('scroll', () => {
+  const currentScroll = window.scrollY;
+  if (currentScroll > scrollPosition) {
+    hideHeadertab();
+  } else {
+    showHeadertab();
+  }
+  scrollPosition = currentScroll;
+});
+
+navLinks.forEach(nav => {
+  nav.addEventListener('click', e => {
+    e.preventDefault();
+    const clicked = e.target.getAttribute('href');
+    const scrollEl = document.querySelector(clicked);
+    scrollEl.scrollIntoView({ behavior: 'smooth' });
+  });
+});
